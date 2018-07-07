6ea6f94502080db557d4e7edc7ccb68638ef6e7b
chris oldfield <caoldfi54@gmail.com>
2018-07-06 22:41:08 -0400

    button

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Button extends Component {
+    render() {
+        return <div className="">button</div>;
+    }
+}
+
+export default Button;
