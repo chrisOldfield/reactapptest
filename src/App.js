3edde92b4afb305d6afe5cbcce47d245c9da0d6a
Matt Oldfield <mattoldfieldweb@gmail.com>
2018-07-06 22:51:46 -0400

    added fetch neo browse

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "./Button";
 
+const API_KEY = "DEMO_KEY";
 class App extends Component {
+    neoBrowseData = {};
+
+    componentDidMount() {
+        // Now that this component mounted, grab the browse data
+        fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse/?api_key=${API_KEY}`)
+            .then(data => {
+                console.log(data);
+                // Set the state so we can use this data later
+                this.setState({ neoBrowseData: data });
+            })
+            .catch(err => {
+                console.log(`Some error happened`, err);
+            });
+    }
     render() {
         return (
             <div className="App">
